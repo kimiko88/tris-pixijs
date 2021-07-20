9c07d9c2eada382e15219cea517b834bd452153b
K <chimien@libero.it>
2021-07-20 13:36:44 +0200

    fix clipboard mobile

--- menu.js
+++ menu.js
@@ -54,6 +54,9 @@ const menu = {
             const el = document.createElement('input')
             input.type = "text";
             el.value = text
+            el.setAttribute('readonly', '')
+            el.style.position = 'absolute'
+            el.style.left = '-9999px'
             document.body.appendChild(el)
             el.select()
             el.setSelectionRange(0, 99999); /* For mobile devices */
